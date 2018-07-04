3bdc532d89aec2105884f8d4f3067ef630898b6b
井野口 りみか <rimika.mimmy727@docomo.ne.jp>
2018-07-04 16:10:47 +0900

    techfile count 1

--- count/ViewController.swift
+++ count/ViewController.swift
@@ -9,17 +9,83 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
+    
+    @IBAction func plusbutton(){
+        
+        number = number + 1
+        label.text = String(number)
+        
+        if number > 10{
+            
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.red
+        }
+    }
+    
+    @IBAction func minusbutton(){
+        
+        number = number - 1
+        label.text = String(number)
+        
+    
+        if number < 0{
+            
+            label.textColor = UIColor.cyan
+        }else{
+            label.textColor = UIColor.red
+        }
+        
+    }
+    
+    @IBAction func multiplybutton(){
+        
+        number = number * 2
+        label.text = String(number)
+        
+        if number > 10{
+            
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.red
+        }
+        
+    }
+    
+    @IBAction func dividebutton(){
+        
+        number = number / 2
+        label.text = String(number)
+        
+        
+        if number < 0{
+            
+            label.textColor = UIColor.cyan
+        }else{
+            label.textColor = UIColor.red
+        }
+    }
+    
+    @IBAction func clearbutton(){
+        
+        number = 0
+        label.text = String(number)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
